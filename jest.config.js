6e8f6e1c5b54aa36040c04c846903db2f57d3a00
José Manuel Lucas <josemanuel@jmlweb.es>
2021-10-21 23:33:39 +0200

    chore: add vscode debugging

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,16 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
-    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    /* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    // Handle CSS imports (without CSS modules)
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    /* Handle files
+    https://jestjs.io/docs/webpack#handling-static-assets */
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    // Alias
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jsdom',
--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@
  **/
 
 const nextConfig = {
-  reactStrictMode: true,
   eslint: {
-    dirs: ['components', 'modules', 'pages', 'scenes'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['components', 'modules', 'pages', 'scenes'],
   },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
